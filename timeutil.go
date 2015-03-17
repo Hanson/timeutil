546dbffffa67d9cac3102e72f9dcd3c2e4f5e576
HanSon <h@hanc.cc>
2015-03-17 23:48:43 +0800

    Update timeutil.go

--- timeutil.go
+++ timeutil.go
@@ -8,55 +8,55 @@ import (
 //  2015-03-17 12:37:06 +0800 CST
 
 //03-17
-func getDate(timeNow time.Time) string {
+func GetDate(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[5:11]
 }
 
 //2015
-func getYear(timeNow time.Time) string {
+func GetYear(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[0:4]
 }
 
 //2015-03
-func getYearWithMon(timeNow time.Time) string {
+func GetYearWithMon(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[0:7]
 }
 
 //17
-func getDay(timeNow time.Time) string {
+func GetDay(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[8:10]
 }
 
 //2015-03-17
-func getFullDate(timeNow time.Time) string {
+func GetFullDate(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[0:11]
 }
 
 //2015-03-17 12
-func getDateWithHour(timeNow time.Time) string {
+func GetDateWithHour(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[0:13]
 }
 
 //2015-03-17 12:37
-func getDateWithTime(timeNow time.Time) string {
+func GetDateWithTime(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[0:16]
 }
 
 //12:37
-func getTime(timeNow time.Time) string {
+func GetTime(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[11:16]
 }
 
 //12:37:06
-func getTimeWithSec(timeNow time.Time) string {
+func GetTimeWithSec(timeNow time.Time) string {
 	times := timeNow.String()
 	return times[11:19]
 }
